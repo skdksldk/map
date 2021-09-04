ceb4776132368f462b50401cc9d21ba9d179c294
skdksldk <tnwns1485@naver.com>
2021-09-04 17:40:52 +0900

    Wi-Fi location check and marker implementation through Seoul Open Data Plaza API

--- src/components/KakaoMap.js
+++ src/components/KakaoMap.js
@@ -1,9 +1,10 @@
 import React, { useState, useEffect, useRef } from 'react';
+import { findWifiLocations } from '../services/wifiAPI';
 const { kakao } = window;
 
 export default function Map({ district, userLocation }) {
     const [map, setMap] = useState(null);
-    const [parkLocation, setParkLocation] = useState([]);
+    const [wifiLocation, setWifiLocation] = useState([]);
     const mapElement = useRef(null);
 
 
@@ -23,10 +24,51 @@ export default function Map({ district, userLocation }) {
         setMap(map);
     }, []);
 
+    useEffect(() => {
+        const fetchData = async() => {
+            try {
+                const locations = await findWifiLocations();
+
+                console.log(locations);
+                setWifiLocation(locations);
+                console.log(3);
+            } catch (error) {
+
+                console.log(error);
+            }
+        };
+        fetchData();
+    }, []);
+
+
+    useEffect(() => {
+        if (map !== null) {
+            const clusterer = new kakao.maps.MarkerClusterer({
+                map: map,
+                averageCenter: true,
+                minLevel: 6,
+            });
+            const markers = wifiLocation.map(el => {
+
+                return new kakao.maps.Marker({
+
+                    map: map,
+
+                    position: new kakao.maps.LatLng(el.WGS84_Y, el.WGS84_X),
+
+                    title: el.NAME_ENG
+                });
+            });
+
+            clusterer.addMarkers(markers);
+        }
+    }, [wifiLocation, map]);
+
 
     return ( <
         div ref = { mapElement }
         style = {
-            { width: '100vw', height: '100vh' } } > < /div>
+            { width: '100vw', height: '100vh' }
+        } > < /div>
     );
 }
